a36a4342a3e7982e4395a18f705f2c88b6a00ead
Fnu Pawan Kumar <pawan.3940@gmail.com>
2023-07-05 17:03:17 -0700

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        fibonacci_list = []
+    elif length==1:
+        fibonacci_list=[0]
+    else:
+        fibonacci_list = [0, 1]  
+        for i in range(2, length):
+            next_fibonacci = fibonacci_list[i - 1] + fibonacci_list[i - 2]  
+            fibonacci_list.append(next_fibonacci)  
+
+    print(fibonacci_list)
+    #pass
